7912961a6c998162ae53d027980867a04bc44511
Pradip <pkra99bux@gmail.com>
2024-07-23 11:20:43 +0530

    array and objects

--- 02_Basics/01_array.js
+++ 02_Basics/01_array.js
@@ -20,3 +20,21 @@ const bikes = ["Yamaha", "Shuzuki", "Harley"]
 const vehicle = cars.concat(bikes)
 
 console.log(vehicle)
+
+// array spread operator can take multiple arrys and concate them
+const all_vehicles = [...cars, ...bikes]
+
+console.log(all_vehicles)
+
+const arr2 = [1, 2, 3, [4, 5], [6, 7, [8, 9]]]
+console.log(arr2.flat(2))
+
+console.log(Array.isArray("Jhon"))
+console.log(Array.from("Jhon"))
+console.log(Array.from({name: "Jhon"})) // passing objects returns empty array
+
+let result1 = 93
+let result2 = 95
+let result3 = 97
+
+console.log(Array.of(result1, result2, result3))
---
+++ 02_Basics/02_objects.js
@@ -0,0 +1,9 @@
+const student1 = {
+    Name: 'Jhon',
+    "Full Name" : "Jhon Doe"
+    Age: 21,
+    Email: 'JhonExample@gmail.com',
+    Address: '1st cross streat, sector 138',
+}
+
+console.log(student1.Name)
