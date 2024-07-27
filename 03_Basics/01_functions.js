50ac2f8bd9ce6109d2e58b652869507a854ec3aa
Pradip <pkra99bux@gmail.com>
2024-07-27 13:19:44 +0530

    all about functions

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -9,6 +9,40 @@ function greetings(username = "Guest") {  //"Guest" is a default parameter
 }
 
 console.log(greetings("jhon"))
-console.log(greetings())                                  //returns undefined or default value when we don't pass the arguemnt
+console.log(greetings())                          //returns undefined or default value when we don't pass the arguemnt
+
+//passing object to the function
+
+const user = {
+    name: "Alice",
+    id: 1231
+}
+
+function welcomMessage(user) {
+    console.log(`Hello, ${user.name} your userid is ${user.id}`)
+}
+
+welcomMessage(user)
+
+welcomMessage({
+    name : "Jhon",
+    id: 2132
+})
+
+//passing array to the function
+
+const myArr = [100, 200, 300]
+
+function arrFirstValue(arr) {
+    return arr[0]
+}
+
+console.log(arrFirstValue(myArr))
+
+const result = function addTwoNumber(num1, num2){
+    return num1 + num2
+}
+
+console.log(sum(10, 20))
 
 
