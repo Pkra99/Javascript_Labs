1f98459adaea07bcf250c03debc2d12b7618f426
Pradip <pkra99bux@gmail.com>
2024-07-27 14:23:39 +0530

    IFFE is fun

---
+++ 03_Basics/03_IIFE.js
@@ -0,0 +1,13 @@
+//Immediately Invoked Function Expresssion (IIFE)
+
+//named IIFE
+(function greeeting () {
+    console.log(`Hello, Good morning`)
+}) ();
+
+
+//unamed or simple IFFE
+((name) => {
+    console.log(`Hello, ${name} Good morning`)
+}) ("Jhon");
+
